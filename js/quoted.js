e4320a9d0a3c07a74e4a946a80ed0222c04ec90f
heisid <weird_alien@protonmail.com>
2018-04-13 09:26:02 +0700

    update quote db

--- js/quoted.js
+++ js/quoted.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
     function generateQuote() {
-        $.getJSON("https://gist.githubusercontent.com/heisid/6663a7f5d3aa77e8c597e7bdc7f3f514/raw/6f01be263ed1cfdf439224b53e2aade9884896d3/quoted.json", function(json) {
-            // Just wondering if I'm doing right.
+        $.getJSON("https://gist.githubusercontent.com/heisid/6663a7f5d3aa77e8c597e7bdc7f3f514/raw/ed1951d80e37e4436379e6706a136a08bbbaadb8/quoted.json", function(json) {
             // console.log("JSON Length = " + json.length);
             var html = "";
             var rnd = Math.floor(Math.random()*(json.length));
@@ -9,6 +8,7 @@ $(document).ready(function() {
                 return val.id == rnd;
             });
             json.forEach(function(val) {
+                // console.log("Quote id: " + val.id);
                 html += "<p id='quote-text'>\"" + val.quote + "\"</p>";
                 html += "<p id='who-text'><em> --" + val.who + "</em></p>";
             });
