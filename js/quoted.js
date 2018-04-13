50ef993ca9910049788caa01b2264a9ae26431c2
heisid <weird_alien@protonmail.com>
2018-04-13 09:09:01 +0700

    update to more various quotes

--- js/quoted.js
+++ js/quoted.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
     function generateQuote() {
-        $.getJSON("https://gist.githubusercontent.com/heisid/6663a7f5d3aa77e8c597e7bdc7f3f514/raw/61c559ef5c5e9f0d8a6e8aa96cb6882eb6c58af7/quoted.json", function(json) {
+        $.getJSON("https://gist.githubusercontent.com/heisid/6663a7f5d3aa77e8c597e7bdc7f3f514/raw/6f01be263ed1cfdf439224b53e2aade9884896d3/quoted.json", function(json) {
+            // Just wondering if I'm doing right.
+            // console.log("JSON Length = " + json.length);
             var html = "";
             var rnd = Math.floor(Math.random()*(json.length));
             json = json.filter(function(val) {
