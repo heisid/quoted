48e9d68feac3a24803eeb539d6a5253df223e31d
Sid <weird_alien@protonmail.com>
2018-04-13 13:03:01 +0700

    add tweet button

--- js/quoted.js
+++ js/quoted.js
@@ -3,16 +3,22 @@ $(document).ready(function() {
         $.getJSON("https://gist.githubusercontent.com/heisid/6663a7f5d3aa77e8c597e7bdc7f3f514/raw/ed1951d80e37e4436379e6706a136a08bbbaadb8/quoted.json", function(json) {
             // console.log("JSON Length = " + json.length);
             var html = "";
+            var quote = "";
+            var who = "";
             var rnd = Math.floor(Math.random()*(json.length));
             json = json.filter(function(val) {
                 return val.id == rnd;
             });
             json.forEach(function(val) {
                 // console.log("Quote id: " + val.id);
-                html += "<p id='quote-text'>\"" + val.quote + "\"</p>";
-                html += "<p id='who-text'><em> --" + val.who + "</em></p>";
+                quote = val.quote;
+                who = val.who;
+                html += "<p id='quote-text'>\"" + quote + "\"</p>";
+                html += "<p id='who-text'><em> --" + who + "</em></p>";
             });
             $(".message").html(html);
+            // %0a is new line, %28 and %29 are ( and ) respectively.
+            $('#tweet').attr('href', 'https://twitter.com/intent/tweet?text='+ quote + "%0a%28" + who +"%29").attr('target', '_blank');
         });
     };
     generateQuote();
