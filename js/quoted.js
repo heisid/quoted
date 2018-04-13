6550a8688c5906fa0194cb592532cbe9ad5d7a89
Sid <weird_alien@protonmail.com>
2018-04-13 07:56:59 +0700

    separate function and the button doesn't work, something is wrong

--- js/quoted.js
+++ js/quoted.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $("#getQuote").on("click",function() {
+    function generateQuote() {
         $.getJSON("https://gist.githubusercontent.com/heisid/6663a7f5d3aa77e8c597e7bdc7f3f514/raw/61c559ef5c5e9f0d8a6e8aa96cb6882eb6c58af7/quoted.json", function(json) {
             var html = "";
             var rnd = Math.floor(Math.random()*(json.length));
@@ -7,10 +7,12 @@ $(document).ready(function() {
                 return val.id == rnd;
             });
             json.forEach(function(val) {
-                html += "<p id='quote-text'>\"" + val.quote + "\"</p>"
-				html += "<p id='who-text'><em> --" + val.who + "</em></p>"
+                html += "<p id='quote-text'>\"" + val.quote + "\"</p>";
+                html += "<p id='who-text'><em> --" + val.who + "</em></p>";
             });
             $(".message").html(html);
         });
-    });
+    };
+    generateQuote();
+    $("#getQuote").on("click", generateQuote());
 });
